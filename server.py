03bb4fd1379ca60d36cdd13aeab6c3e4ce849b54
Ignacio Rueda Rodriguez <irueda@alpha10.aulas.gsyc.urjc.es>
2017-10-27 13:57:44 +0200

    servidor guarda en diccionario la direccion y la ip

--- server.py
+++ server.py
@@ -8,24 +8,30 @@ import socketserver
 import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    diccserv = {}
     def handle(self):
-        """
-        handle method of the server class
-        (all requests will be handled by this method)
-        """
-        self.wfile.write(b"Hemos recibido tu peticion")
-        print (self.client_address)
+        lista = []
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            lista.append(line.decode('utf-8'))
+        
+        messaje = lista[0].split(':')
+        user = messaje[1].split(' ')[0]
+        method = mensaje[0].split(' ')[0]
+        if method == 'REGISTER':
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            IP = self.client_address[0]
+            PORT = self.client_address[1]
+            self.diccserv[user] = (IP + ':' + str(PORT))
+            print(self.diccserv)
+           
 
 if __name__ == "__main__":
     LISTEN_PORT = int(sys.argv[1])
-    serv = socketserver.UDPServer(('', LISTEN_PORT), EchoHandler) 
+    serv = socketserver.UDPServer(('', LISTEN_PORT), SIPRegisterHandler) 
 
     print("Lanzando servidor UDP de eco...")
     try:
