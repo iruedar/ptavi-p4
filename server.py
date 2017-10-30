5c067bfbf78e8c592cf2ad4b3b8b526a2055f8f1
Ignacio Rueda Rodriguez <irueda@gamma16.aulas.gsyc.urjc.es>
2017-10-30 10:38:05 +0100

    mejorado server ejercicio 7 y json añadido

--- server.py
+++ server.py
@@ -1,41 +1,48 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
 
-import socketserver
 import sys
+import json
+import socketserver
+from datetime import datetime, date, time, timedelta
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-
     dicc = {}
+
+    def register2json(self):
+        with open('registered.json', 'w') as json_file:
+            json.dump(self.dicc, json_file, indent=3)
+
     def handle(self):
+
         lista = []
+        IP = self.client_address[0]
+        PORT = self.client_address[1]
+        address = IP + ':' + str(PORT)
         for line in self.rfile:
             lista.append(line.decode('utf-8'))
         
-        IP = self.client_address[0]
-        PORT = self.client_address[1]
-        CLIENT = IP + ':' + str(PORT)
-        message = lista[0].split(':')
-        user = message[1].split(' ')[0]
-        method = message[0].split(' ')[0]
-        expires = lista[1].split('\r\n')[0]
-        expires = expires.split(' ')[1]
+        message = lista[0].split()
+        method = message[0].split()[0]
         if method == 'REGISTER':
-            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-            self.dicc[user] = (CLIENT + str(expires))
+            user = message[1].split(':')[1]
+            message = lista[1].split()
+            expires = message[1].split('\r\n')[0]
+            client = {'address' : address, 'expires' : expires}
+            
         if int(expires) == 0:
             try:
                 del self.dicc[user]
-                print('User', user, 'in', CLIENT, 'deleted\n')
+                print('User', user, 'in', address, 'deleted\n')
             except KeyError:
                 print('User', user, 'not found\n')
         else:
-            print('User', user, 'in', CLIENT)
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            self.dicc[user] = (client)
+            print('User', user, 'in', address)
             print('Expires in', expires, 'seconds\n')
+        self.register2json()
            
 if __name__ == "__main__":
     LISTEN_PORT = int(sys.argv[1])
