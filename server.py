70942c00333200cd1d4021d44b52e7bc29080531
Ignacio Rueda Rodriguez <irueda@alpha27.aulas.gsyc.urjc.es>
2017-11-02 20:08:41 +0100

    ejercicio 8 server

--- server.py
+++ server.py
@@ -4,6 +4,7 @@
 import sys
 import json
 import socketserver
+
 from datetime import datetime, date, time, timedelta
 
 
@@ -20,39 +21,49 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         client = {'address':'', 'expires':''}
         IP = self.client_address[0]
         PORT = self.client_address[1]
+        FORMAT = '%Y-%m-%d %H:%M:%S'
         address = IP + ':' + str(PORT)
         for line in self.rfile:
             lista.append(line.decode('utf-8'))
-        
+
         message = lista[0].split()
         method = message[0].split()[0]
         if method == 'REGISTER':
             user = message[1].split(':')[1]
             message = lista[1].split()
             sec = message[1].split('\r\n')[0]
-            expires = time.time() + int(sec)
-            expired = 
-        if int(sec) == 0:
-            try:
+            expires = datetime.now() + timedelta(seconds=int(sec))
+            date = expires.strftime(FORMAT)
+            client['address'] = address
+            client['expires'] = date
+            if int(sec) == 0:
+                try:
+                    del self.dicc[user]
+                    print('User', user, 'in', address, 'deleted\n')
+                except KeyError:
+                    print('User', user, 'not found\n')
+            else:
+                self.dicc[user] = client
+                print('User', user, 'in', address)
+                print('Expires in', expires)
+            Del_lista = []
+            now = datetime.now().strftime(FORMAT)
+            for user in self.dicc:
+                if self.dicc[user]['expires'] <= now:
+                    Del_lista.append(user)
+            for user in Del_lista:
                 del self.dicc[user]
-                print('User', user, 'in', address, 'deleted\n')
-            except KeyError:
-                print('User', user, 'not found\n')
-        else:
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-            self.dicc[user] = (client)
-            print('User', user, 'in', address)
-            print('Expires in', expires, 'seconds\n')
-        self.register2json()
-           
+            self.register2json()
+
 if __name__ == "__main__":
-    LISTEN_PORT = int(sys.argv[1])
-    serv = socketserver.UDPServer(('', LISTEN_PORT), SIPRegisterHandler) 
 
-    print('Lanzando servidor UDP de eco...\n')
-    try:
-        serv.serve_forever()
-    except KeyboardInterrupt:
-	print("Finalizado servidor")
+	LISTEN_PORT = int(sys.argv[1])
+	serv = socketserver.UDPServer(('', LISTEN_PORT), SIPRegisterHandler)
+	print('Lanzando servidor UDP de eco...\n')
 
-esto es una prueba
+	try:
+		serv.serve_forever()
+	except KeyboardInterrupt:
+		print("Finalizado servidor")
+
